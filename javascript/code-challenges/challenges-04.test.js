bb901a329b6765116fb2d857d50afa352f60c483
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-16 21:33:23 -0700

    completed challenge 6

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -36,9 +36,8 @@ For example: 'Cat' would come before 'apple'
 ------------------------------------------------------------------------------------------------ */
 
 const sortNames = (arr) => {
-  // Solution code here...
-  const newArr = arr.sort();
-  return newArr;
+  // Solution code here....
+  return arr.sort();
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -50,9 +49,8 @@ HINT: Beware... JS default is "Lexical" ordering.
 ------------------------------------------------------------------------------------------------ */
 
 const sortNumbers = (arr) => {
-  // Solution code here...
-  const newArr = arr.sort((a, b) => a - b);
-  return newArr;
+  // Solution code here....
+  return arr.sort((a, b) => a - b);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -65,8 +63,7 @@ HINT: Do it with a custom sort callback, not with using `.reverse()`. ;)
 
 const sortBackwards = (arr) => {
   // Solution code here...
-  const newArr = arr.sort((a, b) => b - a);
-  return newArr;
+  return arr.sort((a, b) => b - a);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -81,8 +78,7 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 
 const alphabetize = (arr) => {
   // Solution code here...
-  const newArr = arr.sort();
-  return newArr;
+  return arr.sort();
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -100,6 +96,7 @@ Here is an example of the input:
 
 const sortByPrice = (arr) => {
   // Solution code here...
+  return arr.sort((a, b) => a.price - b.price);
 };
 
 /* ------------------------------------------------------------------------------------------------
