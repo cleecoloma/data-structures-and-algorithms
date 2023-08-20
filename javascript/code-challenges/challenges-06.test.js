88c53c979240e49a6067b6b0c15eb72b39f113c2
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-19 22:44:48 -0700

    completed challenges 2 thru 4

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -24,10 +24,10 @@ Returns: ['dyoll', 'eimaj'];
 
 const getNames = (arr) => {
   // Solution code here...
-  const newArr = arr.map((element) => element.name.split("").reduce((accumulator, currentValue) => {
+  return arr.map((element) => element.name.split("").reduce((accumulator, currentValue) => {
     return currentValue + accumulator;
   }, ""));
-  return newArr;
+  // return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -39,6 +39,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  return str + ' The end.';
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -56,6 +57,7 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+  arr[arr.length] = arr[0];
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -75,6 +77,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
