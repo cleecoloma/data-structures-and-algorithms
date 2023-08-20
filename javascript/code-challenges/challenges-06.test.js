edccb26ef20ee338cc3a92e285983e16a8dd4d72
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-19 22:51:07 -0700

    completed challenge 6

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -115,7 +115,7 @@ console.log(a) prints [1, 2, 3, 4]
 
 const append = (arr1, arr2) => {
   // Solution code here...
-
+  arr2.forEach(element => arr1.push(element));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -175,7 +175,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
