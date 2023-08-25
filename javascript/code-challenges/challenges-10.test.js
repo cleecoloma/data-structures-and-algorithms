5b6a6e3b3a5192cdd55aeb7f143cca5d3a04dbef
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-24 23:20:30 -0700

    completed challenge 2

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -29,6 +29,15 @@ return: 23
 ------------------------------------------------------------------------------------------------ */
 const findMax = (matrix) => {
   // Solution code here...
+  let number = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j <matrix[i].length; j++) {
+      if (matrix[i][j] > number) {
+        number = matrix[i][j];
+      }
+    }
+  }
+  return number;
 };
 
 /* ------------------------------------------------------------------------------------------------
