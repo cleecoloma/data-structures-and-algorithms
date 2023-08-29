6ea351c11ad734da2e09db71e5e872861f555a3e
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-28 23:23:19 -0700

    completed challenge 5

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -73,6 +73,8 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 
 const validateEmail = (email) => {
   // Solution code here...
+  const pattern = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.(com|net|org)$/;
+  return pattern.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
