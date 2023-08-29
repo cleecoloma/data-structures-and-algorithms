16c810e924a73276bad47d6de2f32f2d3201e5bc
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-28 18:49:12 -0700

    completed challenge 2

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -23,6 +23,8 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 
 const validatePin = (pin) => {
   // Solution code here...
+  const pattern = /^\d{4}$/;
+  return pattern.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
