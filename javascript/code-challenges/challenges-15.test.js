36fa47d55edae8b945cf0fb64239b111cdad92f8
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-31 21:35:37 -0700

    completed challenge 4

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -142,6 +142,8 @@ This data could be sorted by name or price.
 
 const sortBy = (property, arr) => {
   // Solution code here...
+  arr.sort((a, b) => a[property] - b[property]);
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
