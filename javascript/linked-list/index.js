d04d79e43edc3793f134bbc12fbc5f66aa9c06b8
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-22 23:04:55 -0700

    added class linkedlist and node

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,45 @@
 'use strict';
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insert(value) {
+    let current = this.head;
+    let newNode = new Node(value);
+    newNode.next = current;
+  }
+
+  include(value) {
+    let current = this.head;
+    let isValueIncluded = false;
+    while (current.next) {
+      current = current.next
+      if (current.value === value) {
+        isValueIncluded = true;
+        break;
+      }
+    }
+    return isValueIncluded;
+  }
+
+  toString() {
+    let current = this.head;
+    let stringToLog = '';
+    while (current) {
+      stringToLog.concat(`{ ${current.value} } -> `);
+    }
+    stringToLog.concat('NULL');
+  }
 }
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 
 module.exports = LinkedList;
