50c6b6c0da41289c17a2e285f93238009cdb8dac
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-07 22:42:49 -0700

    added Node and BinaryTree

---
+++ javascript/trees/trees.js
@@ -0,0 +1,75 @@
+"use strict";
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  preOrder() {
+    let order = [];
+
+    const traverse = (current) => {
+      order.push(current.value);
+
+      if (current.left) {
+        traverse(current.left);
+      }
+
+      if (current.right) {
+        traverse(current.right);
+      }
+    };
+
+    traverse(this.root);
+
+    return order;
+  }
+
+  inOrder() {
+    let order = [];
+
+    const traverse = (current) => {
+      if (current.left) {
+        traverse(current.left);
+      }
+
+      order.push(current.value);
+
+      if (current.right) {
+        traverse(this.root);
+      }
+
+      traverse(this.root);
+
+      return order;
+    };
+  }
+
+  postOrder() {
+    let order = [];
+
+    const traverse = (current) => {
+      if (current.left) {
+        traverse(current.left);
+      }
+
+      if (current.right) {
+        traverse(current.right);
+      }
+
+      order.push(current.value);
+    };
+
+    traverse(this.root);
+
+    return order;
+  }
+}
