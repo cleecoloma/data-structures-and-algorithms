ccaece3a6f49c984a79e0090c72fdecffa61e1db
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-27 22:29:26 -0700

    added hash method

--- javascript/hash-table/hash-table.js
+++ javascript/hash-table/hash-table.js
@@ -1,33 +1,20 @@
 "use strict";
 
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+const LinkedList = require('../linked-list');
+
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.buckets = new Array(size);
   }
-
-  const mid = Math.floor(arr.length / 2);
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid);
-
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-function merge(left, right) {
-  let result = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
+  hash(key) {
+    let string = JSON.stringify(key);
+    let sum = 0;
+    for (let char of string) {
+      sum += char.charCodeAt(0);
     }
+    let largeNum = sum * 599;
+    let hash = largeNum % this.size;
+    return hash;
   }
-
-  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
-
-module.exports = mergeSort;
