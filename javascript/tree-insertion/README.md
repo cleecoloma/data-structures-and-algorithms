92fd06e98e2604a8364b607bf737a3591fbea57e
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-31 13:38:28 -0700

    added tree intersection function

---
+++ javascript/tree-insertion/README.md
@@ -0,0 +1,37 @@
+# Challenge Title
+## Hash Table
+> This coding challenge is to implement hash table.
+
+### Whiteboard Process
+![Hash Table](../images/hash-table.png)
+
+### Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+1. set(key, value)
+  * Time Complexity - O(n)
+  * Space Complexity - O(1)
+2. get(key)
+  * Time Complexity - O(n)
+  * Space Complexity - O(1)
+3. has(key)
+  * Time Complexity - O(n)
+  * Space Complexity - O(1)
+4. keys
+  * Time Complexity - O(n)
+  * Space Complexity - O(n)
+
+### Solution
+<!-- Show how to run your code, and examples of it in action -->
+[Link to code](https://github.com/cleecoloma/data-structures-and-algorithms/tree/main/javascript/hash-table)
+
+```text
+npm test
+```
+
+### Test
+![Hash Table](../images/hash-table-test.png)
+
+### Collaborators
+Chester Lee Coloma
+ChatGTP (tests)
---
+++ javascript/tree-insertion/__tests__/tree-insertion.test.js
@@ -0,0 +1,53 @@
+"use strict";
+const HashTable = require("../hash-table.js");
+// const LinkedList = require("../../linked-list/index.js");
+
+describe("Hash Table class", () => {
+  test("Setting a key/value to your hashtable results in the value being in the data structure", () => {
+    const myHashTable = new HashTable(100);
+    myHashTable.set("name", "John");
+    expect(myHashTable.get("name")).toBe("John");
+  });
+
+  test("Retrieving based on a key returns the value stored", () => {
+    const myHashTable = new HashTable(100);
+    myHashTable.set("age", 30);
+    expect(parseInt(myHashTable.get("age"))).toBe(30);
+  });
+
+  test("Successfully returns null for a key that does not exist in the hashtable", () => {
+    const myHashTable = new HashTable(100);
+    expect(myHashTable.get("nonexistent")).toBe(null);
+  });
+
+  test("Successfully returns a list of all unique keys that exist in the hashtable", () => {
+    const myHashTable = new HashTable(100);
+    myHashTable.set("name", "John");
+    myHashTable.set("age", 30);
+    myHashTable.set("city", "New York");
+    expect(myHashTable.keys()).toEqual(["name", "age", "city"]);
+  });
+
+  test("Successfully handle a collision within the hashtable", () => {
+    const myHashTable = new HashTable(100);
+    myHashTable.set("a", "value1");
+    myHashTable.set("b", "value2");
+    expect(myHashTable.get("a")).toBe("value1");
+    expect(myHashTable.get("b")).toBe("value2");
+  });
+
+  test("Successfully retrieve a value from a bucket within the hashtable that has a collision", () => {
+    const myHashTable = new HashTable(100);
+    myHashTable.set("a", "value1");
+    myHashTable.set("b", "value2");
+    expect(myHashTable.get("a")).toBe("value1");
+    expect(myHashTable.get("b")).toBe("value2");
+  });
+
+  test("Successfully hash a key to an in-range value", () => {
+    const myHashTable = new HashTable(100);
+    const hash = myHashTable.hash("exampleKey");
+    expect(hash).toBeGreaterThanOrEqual(0);
+    expect(hash).toBeLessThan(100);
+  });
+});
