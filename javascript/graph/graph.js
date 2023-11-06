1d68d8c8c6f6a7e7a0be2d9b9d335f3927a02c2a
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-05 20:24:56 -0800

    added addEdge and addVertex methods

--- javascript/graph/graph.js
+++ javascript/graph/graph.js
@@ -12,3 +12,18 @@ class Edge {
     this.weight = weight;
   }
 }
+
+addVertex(value) {
+  let vertex = new Vertex(value);
+  this.adjacencyList.set(vertex, []);
+  return vertex;
+}
+
+addEdge(startVertex, endVertex, weight = 0) {
+  if (!this.adjacencyList.get(startVertex) || !this.adjacencyList.get(endVertex)) {
+    throw new Error('Invalid vertices!')
+  }
+
+  const adjacencies = this.adjacencyList.get(startVertex);
+  adjacencyList.push(new Edge(endVertex, weight));
+}
